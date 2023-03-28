b5aff4c210bfaece3da2d2c1ec6e90de34030374
momonastia~ <anastasiatyrikova@gmail.com>
2023-03-29 00:57:06 +0200

    Dynamic ID film page logic added

--- src/pages/films/[id].js
+++ src/pages/films/[id].js
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export const getStaticPaths = async () => {
   const res = await fetch(
-    "https://my-json-server.typicode.com/momonastia/nextjs-restaurant/items"
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=9b17c70efd646681f04f06f1a3fc40ed&page=1"
   );
   const data = await res.json();
 
-  const paths = data.map((film) => {
+  const paths = data.results.map((film) => {
     return {
-      params: { id: film.id },
+      params: { id: film.id.toString() },
     };
   });
 
@@ -36,20 +36,20 @@ const Details = ({ film }) => {
   console.log("details film >>>>", film);
 
   return (
-    <div className={styles.singlePizza}>
+    <div className={styles.singleFilm}>
       <h1>{film.original_title}</h1>
-      {/* <div className={styles.imageContainer}>
+      <div className={styles.imageContainer}>
         <Image
-          src={`${pizza.image}`}
-          alt={`${pizza.name}`}
+          src={`https://image.tmdb.org/t/p/w500${film.backdrop_path}`}
+          alt={`${film.title}`}
           width="250"
           height="150"
           Layout="responsive"
         ></Image>
       </div>
       <div>
-        <p>{pizza.desc}</p>
-      </div> */}
+        <p>{film.overview}</p>
+      </div>
     </div>
   );
 };
--- src/pages/films/index.js
+++ src/pages/films/index.js
@@ -5,16 +5,17 @@ import styles from "@/styles/films.module.scss";
 
 export const getStaticProps = async () => {
   const res = await fetch(
-    "https://api.themoviedb.org/3/movie/550?api_key=9b17c70efd646681f04f06f1a3fc40ed"
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=9b17c70efd646681f04f06f1a3fc40ed&page=1"
   );
   const data = await res.json();
 
   return {
-    props: { films: data },
+    props: { films: data.results },
   };
 };
 
 const Films = ({ films }) => {
+  console.log(films);
   console.log(films);
   return (
     <>
@@ -24,36 +25,34 @@ const Films = ({ films }) => {
       </Head>
       <div>
         <h1 className={styles.cardsTitle}>Our films</h1>
-        {/*    <div className={styles.cardsContainer}>
+        <div className={styles.cardsContainer}>
           {films.map((film, i) => {
-            return {
-              <Link href={`/films/${film.id}`} key={pizza.id}>
-                <div
-                  className={styles.pizzaCard}
-                  
-                >
+            return (
+              <Link href={`/films/${film.id}`} key={film.id}>
+                <div className={styles.pizzaCard}>
                   <div className={styles.imageContainer}>
-                   <Image
-                      src={`${pizza.image}`}
-                      alt={`${pizza.name}`}
+                    <Image
+                      src={`https://image.tmdb.org/t/p/w500${film.backdrop_path}`}
+                      alt={`${film.title}`}
                       width="250"
                       height="150"
                       Layout="responsive"
-                    ></Image> 
+                    ></Image>
                   </div>
                   <div className={styles.pizzaInfo}>
-                    <h4>{pizza.name}</h4>
+                    <h4>{film.title}</h4>
                     <p>
-                      {pizza.desc.length > 50
-                        ? `${pizza.desc.substring(0, 50)}...`
-                        : pizza.desc}{" "}
+                      {film.overview.length > 50
+                        ? `${film.overview.substring(0, 50)}...`
+                        : film.overview}
                     </p>
+                    <div>Vote: {film.vote_average}</div>
                   </div>
                 </div>
               </Link>
-            };
+            );
           })}
-        </div> */}
+        </div>
       </div>
     </>
   );
