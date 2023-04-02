db640e5d744cbb0ad9dbdf341986a7eb6afddc6d
momonastia~ <anastasiatyrikova@gmail.com>
2023-04-02 10:54:35 +0200

    Fix: styles

--- src/pages/films/index.js
+++ src/pages/films/index.js
@@ -45,29 +45,15 @@ const Films = ({ filmsSala1, filmsSala2 }) => {
           {filmsSala2.map((film, i) => {
             return (
               <Link href={`/films/${film.id}`} key={film.id}>
-                <div className={styles.filmCard}>
-                  <div className={styles.imageContainer}>
-                    <Image
-                      src={`https://image.tmdb.org/t/p/w500${film.backdrop_path}`}
-                      alt={`${film.title}`}
-                      width="250"
-                      height="150"
-                      Layout="responsive"
-                    ></Image>
-                  </div>
-                  <div className={styles.filmInfo}>
-                    <h4>{film.title}</h4>
-                    <p>
-                      {film.overview.length > 50
-                        ? `${film.overview.substring(0, 50)}...`
-                        : film.overview}
-                    </p>
-                    <div>Vote: {film.vote_average}</div>
-                  </div>
-                </div>
+                <FilmCard film={film} />
               </Link>
             );
           })}
+        </div>
+        <div className={styles.cardsContainer}>
+          <Link href="/" className={styles.btn}>
+            Alla Home
+          </Link>
         </div>
       </div>
     </>
